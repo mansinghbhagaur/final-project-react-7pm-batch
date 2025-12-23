874e27e280503e8099d756b35744417c5753962d
mansingh bhagaur <mansinghbhagaur1@gmail.com>
2025-12-23 08:10:22 +0530

    hero section design and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
+import { Box, Grid, List, ListItem, Typography } from "@mui/material";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  ) 
-}
-
+    <Box sx={{ backgroundColor: "grey", p: 3 }}>
+      <Grid container spacing={1}>
+        {/* frist column */}
+        <Grid size={{xs: 12, sm: 6, md: 6, lg: 4}} color="white">
+          <Typography variant="h4" fontWeight="bold" color="white" mb={2} >Logo</Typography>
+          <Typography variant="body2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis error, minima dolor amet et ullam optio officia modi magni sapiente?</Typography>
+        </Grid>
+        {/* second column */}
+        <Grid size={{xs: 12, sm: 6, md: 6, lg: 4}} sx={{color: "white"}}>
+          <Typography variant="h5" fontWeight="bolder" align="left">Quick Links</Typography>
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Contact</ListItem>
+            <ListItem>About</ListItem>
+          </List>
+        </Grid>
+        {/* column third */}
+        <Grid size={{xs: 12, sm: 6, md: 6, lg: 4}} sx={{color: 'white'}}>
+          <Typography>Hello man</Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
--- src/pages/website/Home.jsx
+++ src/pages/website/Home.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { Box, Button, Stack, Typography } from "@mui/material";
+import React from "react";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          background: '#dbebeaff',
+          paddingTop: 5,
+          paddingBottom: 5
+        }}
+      >
+        <Typography variant="h5" fontWeight="bold">
+          Hero Section
+        </Typography>
+        <Typography variant="p" sx={{margin: "15px 0px 20px 0px", width: {xs: '100%', sm: '50%'},  textAlign: 'center'}}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit optio
+          esse molestiae laudantium blanditiis tenetur possimus ea illum velit
+          in!
+        </Typography>
+        <Stack spacing={2} direction="row">
+          <Button variant="contained" size="small">Primary</Button>
+          <Button variant="outlined" size="small">Primary</Button>
+        </Stack>
+      </Box>
+    </>
+  );
+};
